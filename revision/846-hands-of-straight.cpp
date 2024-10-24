9253b9dd4cb25b2e07ca70735adf16f35a4ab2e1
sneha gupta <gsneha20012004@gmail.com>
2024-10-24 09:56:49 +0530

    revised the ques hands of straight. PQ MED

--- revision/846-hands-of-straight.cpp
+++ revision/846-hands-of-straight.cpp
@@ -1,32 +1,82 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int n) {
-        int size = hand.size();
-        if(size % n != 0) return false;
+        if(hand.size() % n != 0) return false;
 
         map<int,int> freq;
-
         for(int i=0;i<hand.size();i++){
             freq[hand[i]]++;
         }
 
-
         while(!freq.empty()){
             int i = freq.begin()->first;
             for(int j=0;j<n;j++){
-                // next consecutive element is not present.
-                if(freq[i+j] == 0) {
-                    return false;
-                }
-
+                if(!freq[i+j]) return false;
                 freq[i+j]--;
-
-                if(freq[i+j]<1){
-                    freq.erase(i+j);
-                }
+                if(freq[i+j] < 1) freq.erase(i+j);
             }
         }
 
         return true;
     }
 };
+
+
+
+// - ordered map : freq
+// - or maybe pq<map>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+int size = hand.size();
+if(size % n != 0) return false;
+
+map<int,int> freq;
+
+for(int i=0;i<hand.size();i++){
+    freq[hand[i]]++;
+}
+
+
+while(!freq.empty()){
+    int i = freq.begin()->first;
+    for(int j=0;j<n;j++){
+        // next consecutive element is not present.
+        if(freq[i+j] == 0) {
+            return false;
+        }
+
+        freq[i+j]--;
+
+        if(freq[i+j]<1){
+            freq.erase(i+j);
+        }
+    }
+}
+
+return true;
+
+*/
