8dc894f66437e9825975fd4449a1cc656a1d78ba
sneha gupta <gsneha20012004@gmail.com>
2024-10-28 10:56:13 +0530

    revised 'design twitter' problem. beats 100%

--- revision/355-design-twitter.cpp
+++ revision/355-design-twitter.cpp
@@ -1,6 +1,80 @@
 class Twitter {
 public:
 
+    int timestamp;
+    map<int, vector<pair<int,int>>> tweets;
+    map<int, vector<int>> following;
+
+    Twitter() {
+        timestamp = 0;
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        timestamp++;
+        tweets[userId].push_back({timestamp, tweetId});
+        return;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        priority_queue<pair<int,int>> pq;
+        vector<pair<int,int>> vec = tweets[userId];
+        int size = vec.size();
+
+        for(int i=0;i<size;i++){
+            pq.push(vec[i]);
+        }
+
+        vector<int> temp = following[userId];
+        for(int i=0;i<temp.size();i++){
+            vec = tweets[temp[i]];
+            for(int i=0;i<vec.size();i++){
+                pq.push(vec[i]);
+            }
+            
+        }
+
+        vector<int> ans;
+        while(ans.size()<10 && !pq.empty()){
+            pair<int,int> p = pq.top();
+            ans.push_back(p.second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        vector<int> vec = following[followerId];
+        auto it = find(vec.begin(), vec.end(), followeeId);
+        if(it == vec.end()){
+            following[followerId].push_back(followeeId);
+        }
+        return;
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+
+        following[followerId].erase(remove(following[followerId].begin(), following[followerId].end(), followeeId), following[followerId].end());
+
+        return;
+    }
+};
+
+/*
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+*/
+
+
+
+/*
+class Twitter {
+public:
+
     // map<userid, num of tweets>
     map<int,int> numOfTweets;
 
@@ -86,11 +160,13 @@ public:
 
 };
 
-/**
+*/
+
+/*
  * Your Twitter object will be instantiated and called as such:
  * Twitter* obj = new Twitter();
  * obj->postTweet(userId,tweetId);
  * vector<int> param_2 = obj->getNewsFeed(userId);
  * obj->follow(followerId,followeeId);
  * obj->unfollow(followerId,followeeId);
- */
+*/
