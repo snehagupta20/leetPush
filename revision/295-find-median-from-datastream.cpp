9a8673f44024db40d3f9864c61b41b6d620c42b5
sneha gupta <gsneha20012004@gmail.com>
2024-10-29 09:03:41 +0530

    295 lc revised pq med

--- revision/295-find-median-from-datastream.cpp
+++ revision/295-find-median-from-datastream.cpp
@@ -1,5 +1,43 @@
 class MedianFinder {
 public:
+    priority_queue<int> left;
+    priority_queue<int, vector<int>, greater<int>> right;
+
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        left.push(num);
+
+        right.push(left.top());
+        left.pop();
+
+        if(left.size() < right.size()){
+            left.push(right.top());
+            right.pop();
+        }    
+
+        return;
+    }
+    
+    double findMedian() {
+        if( (left.size() + right.size()) % 2 == 0 ){
+            return (left.top()+right.top())/2.0;
+        } 
+        return left.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
+/*
+class MedianFinder {
+public:
     priority_queue<int> maxheap;
     priority_queue<int, vector<int>, greater<int>> minheap;
 
@@ -30,6 +68,7 @@ public:
         return ( maxheap.top() + minheap.top() ) / 2.0;
     }
 };
+*/
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
