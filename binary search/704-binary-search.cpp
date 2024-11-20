da263cba6965ed52cb0fe5dc910bd6abba2001f2
sneha gupta <gsneha20012004@gmail.com>
2024-11-20 11:18:43 +0530

    binary search basic sol

--- binary search/704-binary-search.cpp
+++ binary search/704-binary-search.cpp
@@ -22,3 +22,29 @@ public:
         return binarySearch(nums,target,0,nums.size()-1);
     }
 };
+
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size()-1;
+
+        while(start<=end){
+            int mid = ((end-start)/2)+start;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start = mid+1;
+            }
+            else if(nums[mid]>target){
+                end = mid-1;
+            }
+        }
+
+        return -1;
+    }
+};
+
