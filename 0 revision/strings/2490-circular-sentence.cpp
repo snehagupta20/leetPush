9c8e841e5e68e87e6b6a39c597671b7238606e8b
sneha gupta <gsneha20012004@gmail.com>
2025-02-03 13:19:10 +0530

    strings circular, out of rev !!!!!

--- 0 revision/strings/2490-circular-sentence.cpp
+++ 0 revision/strings/2490-circular-sentence.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool isCircularSentence(string s) {
-        if(s[0]!=s[s.size()-1]) return false;
+    bool isCircularSentence(string sentence) {
+        if(sentence[0] != sentence[sentence.size()-1]) return false;
 
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' '){
-                if(s[i-1]!=s[i+1]) return false;
+        for(int i=0;i<sentence.size();i++){
+            if(sentence[i]== ' '){
+                if(sentence[i-1]!=sentence[i+1]) return false;
             }
         }
 
