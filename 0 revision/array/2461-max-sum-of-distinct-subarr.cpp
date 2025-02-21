4e333de68d6a0044048b8eeae9900d79e0e2b74a
sneha gupta <gsneha20012004@gmail.com>
2025-02-21 11:48:18 +0530

    array revision done

--- 0 revision/array/2461-max-sum-of-distinct-subarr.cpp
+++ 0 revision/array/2461-max-sum-of-distinct-subarr.cpp
@@ -1,28 +1,29 @@
 class Solution {
-public:
-    long long maximumSubarraySum(vector<int>& nums, int k) {
-        map<int, int> freq; 
-        long long maxSum = 0, currentSum = 0;
-        int start = 0;
-
-        for (int end = 0; end < nums.size(); end++) {
-            freq[nums[end]]++;
-            currentSum += nums[end];
-
-            if (end - start + 1 > k) {
-                freq[nums[start]]--;
-                if (freq[nums[start]] == 0) {
-                    freq.erase(nums[start]);
+    public:
+        long long maximumSubarraySum(vector<int>& nums, int k) {
+            map<int,int> freq;
+            long long currsum=0;
+            int end=0;
+            int start=0;
+            long long sum =0;
+            for(end=0;end<nums.size();end++){
+                freq[nums[end]]++;
+                currsum+=nums[end];
+    
+                if(end - start + 1 > k){
+                    freq[nums[start]]--;
+                    if(freq[nums[start]]==0){
+                        freq.erase(nums[start]);
+                    }
+                    currsum-=nums[start];
+                    start++;
                 }
-                currentSum -= nums[start];
-                start++;
+    
+                if(end - start + 1 == k && freq.size()==k){
+                    sum=max(sum, currsum);
+                }
             }
-
-            if (end - start + 1 == k && freq.size() == k) {
-                maxSum = max(maxSum, currentSum);
-            }
+    
+            return sum;
         }
-
-        return maxSum;
-    }
-};
+    };
--- 0 revision/array/846-hands-of-straight.cpp
+++ 0 revision/array/846-hands-of-straight.cpp
@@ -1,82 +1,22 @@
 class Solution {
-public:
-    bool isNStraightHand(vector<int>& hand, int n) {
-        if(hand.size() % n != 0) return false;
-
-        map<int,int> freq;
-        for(int i=0;i<hand.size();i++){
-            freq[hand[i]]++;
-        }
-
-        while(!freq.empty()){
-            int i = freq.begin()->first;
-            for(int j=0;j<n;j++){
-                if(!freq[i+j]) return false;
-                freq[i+j]--;
-                if(freq[i+j] < 1) freq.erase(i+j);
+    public:
+        bool isNStraightHand(vector<int>& hand, int groupSize) {
+            if(hand.size() % groupSize != 0) return false;
+    
+            map<int,int> freq;
+            for(int i=0;i<hand.size();i++){
+                freq[hand[i]]++;
             }
+    
+            while(freq.size() > 0){
+                int i=freq.begin()->first;
+                for(int j=0;j<groupSize;j++){
+                    if(!freq[i+j]) return false;
+                    freq[i+j]--;
+                    if(freq[i+j] < 1) freq.erase(i+j);
+                }
+            }
+    
+            return true;
         }
-
-        return true;
-    }
-};
-
-
-
-// - ordered map : freq
-// - or maybe pq<map>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-int size = hand.size();
-if(size % n != 0) return false;
-
-map<int,int> freq;
-
-for(int i=0;i<hand.size();i++){
-    freq[hand[i]]++;
-}
-
-
-while(!freq.empty()){
-    int i = freq.begin()->first;
-    for(int j=0;j<n;j++){
-        // next consecutive element is not present.
-        if(freq[i+j] == 0) {
-            return false;
-        }
-
-        freq[i+j]--;
-
-        if(freq[i+j]<1){
-            freq.erase(i+j);
-        }
-    }
-}
-
-return true;
-
-*/
+    };
